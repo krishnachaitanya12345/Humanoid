f6c5a1e8ca0df6a4ced0a9a0bbdcdf3241469f1b
Krishna Chaitanya Singarapu <46423726+krishnachaitanya12345@users.noreply.github.com>
2022-10-16 17:49:09 -0400

    Update finger.ino

--- finger/finger.ino
+++ finger/finger.ino
@@ -1,14 +1,3 @@
-/* Sweep
-  by BARRAGAN <http://barraganstudio.com>
-  This example code is in the public domain.
-
-  modified 28 May 2015
-  by Michael C. Miller
-  modified 8 Nov 2013
-  by Scott Fitzgerald
-
-  http://arduino.cc/en/Tutorial/Sweep
-*/
 
 #include <Servo.h>
 
